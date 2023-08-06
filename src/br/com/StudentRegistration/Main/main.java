f9359b84a1f4a741031982fc6ac3036e4c495d41
Guilherme <guilhermebauer16@gmail.com>
2023-08-06 19:30:58 -0300

    Create project archive

---
+++ src/br/com/StudentRegistration/Main/main.java
@@ -0,0 +1,10 @@
+package br.com.StudentRegistration.Main;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
